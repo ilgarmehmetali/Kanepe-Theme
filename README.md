0bc0cc02d5a6486387d005b0172e2d997e8b4644
Mehmet Ali İLGAR <mehmet.ali@milgar.net>
2016-04-25 03:54:37 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Nikola theme
+
+This is theme from personal blog [milgar.net](https://www.milgar.net/).
+
+
+Licensed under MIT
---
+++ messages/messages_en.py
@@ -0,0 +1,4 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES['LANGUAGE CODE'] = '<span class="fa-stack" style="font-size:0.7em"><i class="fa fa-square-o fa-stack-2x"></i><strong class="fa-stack-1x" style="font-size: 0.8em;">EN</strong></span>'
